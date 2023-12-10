e2c3b3a3c2d0708bdedb3c2ae84ac509ba03f2e5
Shashank Dhauni <112565970+Shashank-Develops@users.noreply.github.com>
2023-12-10 22:29:38 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (64.07%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int>res(num+1);
+        res[0] = 0;
+        for(int i = 1 ; i <= num ; i++)
+            res[i] = res[i/2] + (i&1);
+        return res;
+    }
+};
